d2e371a358ecdd11b7fd1dafdf12824bb6c77dfa
maritzavst <maritzasintitafur@gmail.com>
2019-02-23 11:34:57 -0500

    loader were installed

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,25 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+	NODE_ENV: '"development"',
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+				'vue-style-loader',
+				'css-loader',
+				'sass-loader'
+				]
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug-plain-loader'
+			},
+			{
+				test: /\.yaml$/,
+				loader: 'yaml-loader'
+			},
+		]
+	},
 })
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+	#app
+		img(src="./assets/logo.png")
+		h1 {{ title }}
+		router-view
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	export default {
+		name: 'App',
+		data() {
+			return {
+				title: "GraphQL App"
+			}
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style <style lang="sass">
+	#app 
+		font-family: 'Avenir', Helvetica, Arial, sans-serif
+		-webkit-font-smoothing: antialiased
+		-moz-osx-font-smoothing: grayscale
+		text-align: center
+		color: #2c3e50
+		margin-top: 180px
+
 </style>
