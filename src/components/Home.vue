c1cb7662d0e7a666414cd3d9d2112d4a214b82e6
Maritza Victoria Sinti Tafur <maritza.sinti@tismart.com>
2019-02-25 17:01:58 -0500

    last changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -29,8 +29,8 @@
 							p We couldn’t find any users matching "{{ search }}"
 							img(src="../assets/images/no-data.jpg" alt="No data image")
 						loader(v-if="loading")
-					//- .text-xs-center.pagination-container(v-if="users.length")
-					//- 	v-pagination(v-model='page', :length='length', :total-visible='4' @previous="changePage(false)" @next="changePage(true)")
+					.text-xs-center.pagination-container(v-if="users.length")
+						v-pagination(v-model='page', :length='length', :total-visible='4' @previous="changePage(false)" @next="changePage(true)")
 
 </template>
 
