83b4d629af4bade6e53b0f6a696992bef36454c7
maritzavst <maritzasintitafur@gmail.com>
2019-02-25 04:21:05 -0500

    Location icon and image default were added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,16 +11,21 @@
 					v-card-text.list-container 
 						v-list(v-if="dataExist" two-line='')
 							template(v-for='(user, i) in users')
-								v-list-tile(:key='i', avatar='', @click='goUserDetail(user.userName)')
+								v-list-tile(:key='i', avatar='', @click='goUserDetail(user.userName)').user
 									v-list-tile-avatar
-										img(:src='user.avatarUrl')
+										img(v-if="user.avatarUrl" :src='user.avatarUrl' alt="User image")
+										img(v-else src='../assets/images/default-img.png' alt="User image")
 									v-list-tile-content
-										v-list-tile-title {{ user.name }}, {{ user.location }}
+										v-list-tile-title {{ user.name }}
+											span.user__location(v-if="user.location")
+												| ,
+												v-icon place  
+												|  {{ user.location }}
 										v-list-tile-sub-title {{ user.userName }}
 								v-divider
 						div.not-found(v-else)
 							p We couldn’t find any users matching "{{ search }}"
-							img(src="../assets/images/no-data.jpg")
+							img(src="../assets/images/no-data.jpg" alt="No data image")
 						loader(v-if="loading")
 					.text-xs-center.pagination-container(v-if="users.length")
 						v-pagination(v-model='page', :length='length', :total-visible='4' @previous="changePage(false)" @next="changePage(true)")
@@ -89,7 +94,7 @@
 					})
 					this.dataExist = Boolean(this.users.length)
 				} catch(error) {
-					console.log(error)
+					console.log('error', error)
 				}
 				this.loading = false
 				
@@ -141,4 +146,12 @@
 			width: 300px
 		p 
 			font-size: 16px
+
+	.user__location
+		i.v-icon
+			color: black
+			font-size: 16px
+			position: relative
+			top: -3px
+			margin-left: 5px
 </style> 
